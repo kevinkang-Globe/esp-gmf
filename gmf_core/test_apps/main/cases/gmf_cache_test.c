e0de13cdd20b698a86d63d7459a5d4fb660323f0
Gao Wei <gaowei@espressif.com>
2025-04-11 14:21:23 +0800

    feature: Update license

--- gmf_core/test_apps/main/cases/gmf_cache_test.c
+++ gmf_core/test_apps/main/cases/gmf_cache_test.c
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Espressif Systems (Shanghai) CO., LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <stdio.h>
 #include <sys/stat.h>
 #include <string.h>
